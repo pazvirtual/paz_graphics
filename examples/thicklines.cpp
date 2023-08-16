0dd0dad7626cba3dedece3e311e8b40abca135c5
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:49:39 -0500

    Improves geometry shader example.

--- examples/thicklines.cpp
+++ examples/thicklines.cpp
@@ -1,46 +1,58 @@
 #include "PAZ_Graphics"
 
 static const std::string VertSrc = 1 + R"===(
-layout(location = 0) in vec2 aPos;
+layout(location = 0) in vec2 pos;
 void main()
 {
-    gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
+    gl_Position = vec4(pos.x, pos.y, 0, 1);
 }
 )===";
 
 static const std::string GeomSrc = 1 + R"===(
 layout(lines) in;
+uniform int width;
+uniform int height;
 layout(triangle_strip, max_vertices = 4) out;
+const float s = 2;
 void main()
 {
-    gl_Position = gl_in[0].gl_Position + vec4(0,-0.1, 0, 0);
+    vec2 para = (gl_in[1].gl_Position.xy - gl_in[0].gl_Position.xy);
+    para /= length(para);
+    vec2 perp = vec2(-para.y, para.x);
+    para /= vec2(width, height);
+    perp /= vec2(width, height);
+    gl_Position = vec4(gl_in[0].gl_Position.xy - s*perp, 0, 1);
     EmitVertex();
-    gl_Position = gl_in[1].gl_Position + vec4(0,-0.1, 0, 0);
+    gl_Position = vec4(gl_in[1].gl_Position.xy - s*perp, 0, 1);
     EmitVertex();
-    gl_Position = gl_in[0].gl_Position + vec4(0, 0.1, 0, 0);
+    gl_Position = vec4(gl_in[0].gl_Position.xy + s*perp, 0, 1);
     EmitVertex();
-    gl_Position = gl_in[1].gl_Position + vec4(0, 0.1, 0, 0);
+    gl_Position = vec4(gl_in[1].gl_Position.xy + s*perp, 0, 1);
     EmitVertex();
     EndPrimitive();
 }
 )===";
 
 static const std::string FragSrc = 1 + R"===(
-layout(location = 0) out vec4 FragColor;
+layout(location = 0) out vec4 color;
 void main()
 {
-    FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+    color = vec4(0, 1, 0, 1);
 }
 )===";
 
-static constexpr std::array<float, 8> points =
+static constexpr std::array<float, 4*5> points =
 {
-    -0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f, -0.5f, -0.5f
+    0, 0, 0.5f, 0,
+    0.5f, 0, 0, 0.5f,
+    0, 0.5f, -0.5f, 0.5f,
+    -0.5f, 0.5f, -0.5f, -0.5f,
+    -0.5f, -0.5f, 0.5f, -0.5f
 };
 
 int main()
 {
-    paz::Window::SetCursorMode(paz::Window::CursorMode::Normal); //TEMP
+    paz::Window::SetMinSize(640, 480);
 
     paz::VertexBuffer vertices;
     vertices.attribute(2, points);
@@ -57,6 +69,8 @@ int main()
     paz::Window::Loop([&]()
     {
         render.begin({paz::RenderPass::LoadAction::Clear});
+        render.uniform("width", paz::Window::ViewportWidth());
+        render.uniform("height", paz::Window::ViewportHeight());
         render.primitives(paz::RenderPass::PrimitiveType::Lines, vertices);
         render.end();
     }
