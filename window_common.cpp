98a926212b7787de3690530fd9feec83cd33d51a
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:49:40 -0500

    Fixes issue with final framebuffer on Metal builds.

--- window_common.cpp
+++ window_common.cpp
@@ -5,7 +5,7 @@ paz::Framebuffer paz::final_framebuffer()
     static const Framebuffer f = []()
     {
         Framebuffer f;
-        f.attach(RenderTarget(1., TextureFormat::RGBA8UNorm));
+        f.attach(RenderTarget(1., TextureFormat::BGRA8UNorm));
         f.attach(RenderTarget(1., TextureFormat::Depth16UNorm));
         return f;
     }();
