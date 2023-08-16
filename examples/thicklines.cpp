37a31c6cf9d78a5d539e19771d8b48f09191cfff
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:49:39 -0500

    Makes it possible to pass vertex shader outputs to fragment shader when using thick lines.

--- examples/thicklines.cpp
+++ examples/thicklines.cpp
@@ -2,28 +2,32 @@
 
 static const std::string VertSrc = 1 + R"===(
 layout(location = 0) in vec2 pos;
+flat out float a;
 void main()
 {
     gl_Position = vec4(pos.x, pos.y, 0, 1);
     gl_LineWidth = 4.;
+    a = float(gl_VertexID%2);
 }
 )===";
 
 static const std::string FragSrc = 1 + R"===(
 layout(location = 0) out vec4 color;
+flat in float a;
 void main()
 {
-    color = vec4(0, 1, 0, 1);
+    color = vec4(a, 1. - a, 1, 1);
 }
 )===";
 
 static constexpr std::array<float, 4*5> points =
 {
-    0, 0, 0.5f, 0,
-    0.5f, 0, 0, 0.5f,
-    0, 0.5f, -0.5f, 0.5f,
-    -0.5f, 0.5f, -0.5f, -0.5f,
-    -0.5f, -0.5f, 0.5f, -0.5f
+        0,     0,
+     0.5f,     0,
+        0,  0.5f,
+    -0.5f,  0.5f,
+    -0.5f, -0.5f,
+     0.5f, -0.5f
 };
 
 int main()
@@ -47,7 +51,7 @@ int main()
         }
 
         render.begin({paz::LoadAction::Clear});
-        render.primitives(paz::PrimitiveType::Lines, vertices);
+        render.primitives(paz::PrimitiveType::LineStrip, vertices);
         render.end();
 
         paz::Window::EndFrame();
--- shader_function_library.cpp
+++ shader_function_library.cpp
@@ -34,12 +34,79 @@ static bool uses_gl_line_width(const std::string& vertSrc)
     return false;
 }
 
-static const std::string ThickLinesGeomSrc = 1 + R"===(
+std::string prep_for_geom(const std::string& vertSrc, std::string& block, std::
+    string& block0, std::string& block1)
+{
+    std::unordered_map<std::string, std::pair<std::string, bool>> outputs;
+    {
+        std::istringstream iss(vertSrc);
+        std::string line;
+        while(std::getline(iss, line))
+        {
+            line = std::regex_replace(line, std::regex("//.*$"), "");
+            line = std::regex_replace(line, std::regex("\\s+$"), "");
+            if(line.empty())
+            {
+                continue;
+            }
+            if(std::regex_match(line, std::regex("out\\s.*")))
+            {
+                const std::string dec = line.substr(4, line.size() - 5);
+                const std::size_t pos = dec.find_last_of(' ');
+                const std::string type = dec.substr(0, pos);
+                const std::string name = dec.substr(pos + 1);
+                outputs[name] = {type, false};
+                continue;
+            }
+            if(std::regex_match(line, std::regex("flat out\\s.*")))
+            {
+                const std::string dec = line.substr(9, line.size() - 10);
+                const std::size_t pos = dec.find_last_of(' ');
+                const std::string type = dec.substr(0, pos);
+                const std::string name = dec.substr(pos + 1);
+                outputs[name] = {type, true};
+                continue;
+            }
+        }
+    }
+    if(outputs.empty())
+    {
+        return vertSrc;
+    }
+
+    std::string regexStr;
+    for(const auto& n : outputs)
+    {
+        regexStr += "|" + n.first;
+        block += std::string(n.second.second ? "flat " : "") + "in " + n.
+            second.first + " inout_" + n.first + "[];\n" + (n.second.second ?
+            "flat " : "") + "out " + n.second.first + " " + n.first + ";\n";
+        block0 += n.first + " = inout_" + n.first + "[0];\n";
+        block1 += n.first + " = inout_" + n.first + "[1];\n";
+    }
+    regexStr = "\\b(" + regexStr.substr(1) + ")\\b";
+
+    std::string modifiedSrc;
+    std::istringstream iss(vertSrc);
+    std::string line;
+    while(std::getline(iss, line))
+    {
+        line = std::regex_replace(line, std::regex(regexStr), "inout_$1");
+        modifiedSrc += line + "\n";
+    }
+
+    return modifiedSrc;
+}
+
+static const std::string Src0 = 1 + R"===(
 layout(lines) in;
 uniform int paz_Width;
 uniform int paz_Height;
 layout(triangle_strip, max_vertices = 4) out;
 in float glLineWidth[];
+)===";
+
+static const std::string Src1 = 1 + R"===(
 void main()
 {
     vec2 size = vec2(paz_Width, paz_Height);
@@ -50,15 +117,27 @@ void main()
     float s1 = glLineWidth[1]*gl_in[1].gl_Position.w;
     gl_Position = vec4(gl_in[0].gl_Position.xy - s0*perp, gl_in[0].
         gl_Position.zw);
+)===";
+
+static const std::string Src2 = 1 + R"===(
     EmitVertex();
     gl_Position = vec4(gl_in[1].gl_Position.xy - s1*perp, gl_in[0].
         gl_Position.zw);
+)===";
+
+static const std::string Src3 = 1 + R"===(
     EmitVertex();
     gl_Position = vec4(gl_in[0].gl_Position.xy + s0*perp, gl_in[0].
         gl_Position.zw);
+)===";
+
+static const std::string Src4 = 1 + R"===(
     EmitVertex();
     gl_Position = vec4(gl_in[1].gl_Position.xy + s1*perp, gl_in[0].
         gl_Position.zw);
+)===";
+
+static const std::string Src5 = 1 + R"===(
     EmitVertex();
     EndPrimitive();
 }
@@ -121,9 +200,16 @@ void paz::ShaderFunctionLibrary::vertex(const std::string& name, const std::
         const bool usesGlLineWidth = uses_gl_line_width(src);
         if(usesGlLineWidth)
         {
+            std::string block;
+            std::string block0;
+            std::string block1;
+            const std::string modifiedSrc = prep_for_geom(src, block, block0,
+                block1);
             _data->_vertexIds[name] = compile_shader("#define gl_LineWidth glLi"
-                "neWidth\nout float glLineWidth;\n" + src, GL_VERTEX_SHADER);
-            _data->_thickLinesIds[name] = compile_shader(ThickLinesGeomSrc,
+                "neWidth\nout float glLineWidth;\n" + modifiedSrc,
+                GL_VERTEX_SHADER);
+            _data->_thickLinesIds[name] = compile_shader(Src0 + block + Src1 +
+                block0 + Src2 + block1 + Src3 + block0 + Src4 + block1 + Src5,
                 GL_GEOMETRY_SHADER);
         }
         else
