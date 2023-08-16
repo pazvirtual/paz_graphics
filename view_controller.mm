5646b58de3755de03b8550ec06a4f3d42b085da0
Ben Reifler <ben.reifler@gmail.com>
2023-08-16 16:49:39 -0500

    Changes default framebuffer's depth format again because of an apparent bug in `MTKView`. Note that Metal builds now use `Depth32Float` and OpenGL builds request `Depth32UNorm` (which does not exist in Metal).

--- view_controller.mm
+++ view_controller.mm
@@ -70,7 +70,7 @@
 
     [_mtkView setDelegate:_renderer];
     [_mtkView setClearColor:MTLClearColorMake(0., 0., 0., 0.)];
-    [_mtkView setDepthStencilPixelFormat:MTLPixelFormatDepth16Unorm];
+    [_mtkView setDepthStencilPixelFormat:MTLPixelFormatDepth32Float];
 
     [_renderer mtkView:_mtkView drawableSizeWillChange:[_mtkView drawableSize]];
 }
